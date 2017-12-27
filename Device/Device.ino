b8b0372e31b48c62932b7230f1c88a5687726024
Shrek <busternr@abv.bg>
2017-12-27 19:30:17 +0200

    Refactoring repository folders and more work done on the code for the device

--- Device/Device.ino
+++ Device/Device.ino
@@ -8,11 +8,14 @@ DFRobot_SIM808 sim808(&mySerial);//Connect RX,TX,PWR,
 
 float x;
 float y;
-char deviceId[7];
+long timeout = 60000;
+char deviceId[7] = "23323";
 char latitude[10];
 char longtitude[10];
-//char http_cmd[] = "POST /device/send/gps-cordinates?deviceId=22233&x=4333332&y=223 HTTP/1.0\r\n\r\n";
-char http_cmd[200] = "POST /device/send/gps-cordinates?deviceId=22233&x=";
+//char http_get_cmd[] = "POST /device/22233/receive/device-configuration HTTP/1.0\r\n\r\n";
+//char http_post_cmd[] = "POST /device/send/gps-cordinates?deviceId=22233&x=4333332&y=223 HTTP/1.0\r\n\r\n";
+char http_get_cmd[100] = "GET /device/";
+char http_post_cmd[100] = "POST /device/send/gps-cordinates?deviceId=";
 char buffer[512];
 
 void setup()
@@ -20,12 +23,17 @@ void setup()
   mySerial.begin(9600);
   Serial.begin(9600);
   initializeConnection();
-  delay(3000);  
-  getGPSCordinates();
   checkTCPConnection();
-  sendGPSCordinates();
-  closeConnection();
+  getDeviceConfiguration();
 }
+
+void loop()
+{
+  getGPSCordinates();
+  sendGPSCordinates();
+  delay(timeout);
+}
+
 void initializeConnection()
 {
   while(!sim808.init())
@@ -33,6 +41,7 @@ void initializeConnection()
       delay(1000);
       Serial.print("Sim808 init error\r\n");
   }
+  Serial.print("Sim808 init success\r\n");
 }
 
 void checkTCPConnection()
@@ -51,20 +60,42 @@ void getGPSCordinates()
   Serial.println(sim808.GPSdata.lat);
   Serial.print("Y=");
   Serial.println(sim808.GPSdata.lon);
-  dtostrf(x, 6, 2, longtitude);
-  dtostrf(y, 6, 2, latitude);
+  x = 32;
+  y = 32;
+  dtostrf(x, 2, 0, longtitude);
+  dtostrf(y, 2, 0, latitude);
   sim808.detachGPS();
   Serial.println("Close the GPS power success");
-  strcat(http_cmd, latitude);
-  strcat(http_cmd, "&y=");
-  strcat(http_cmd, longtitude);
-  strcat(http_cmd, " HTTP/1.0\r\n\r\n");
-  Serial.println(http_cmd);
+  strcat(http_post_cmd, deviceId);
+  strcat(http_post_cmd, "&x=");
+  strcat(http_post_cmd, latitude);
+  strcat(http_post_cmd, "&y=");
+  strcat(http_post_cmd, longtitude);
+  strcat(http_post_cmd, " HTTP/1.0\r\n\r\n");
+  Serial.println(http_post_cmd);
+}
+
+void getDeviceConfiguration()
+{
+  strcat(http_get_cmd, deviceId);
+  strcat(http_get_cmd, "/receive/device-configuration HTTP/1.0\r\n\r\n");
+  Serial.println(http_get_cmd);
+  sim808.send(http_get_cmd, sizeof(http_get_cmd)-1);
+  while (true) {
+      int ret = sim808.recv(buffer, sizeof(buffer)-1);
+      if (ret <= 0){
+          Serial.println("fetch over...");
+          break; 
+      }
+      buffer[ret] = '\0';
+      Serial.print(buffer);
+      break;
+  }
 }
 
 void sendGPSCordinates()
 {
-  sim808.send(http_cmd, sizeof(http_cmd)-1);
+  sim808.send(http_post_cmd, sizeof(http_post_cmd)-1);
   while (true) {
       int ret = sim808.recv(buffer, sizeof(buffer)-1);
       if (ret <= 0){
@@ -81,7 +112,4 @@ void closeConnection()
 {
   sim808.close();
   sim808.disconnect();
-}
-void loop()
-{
 }
